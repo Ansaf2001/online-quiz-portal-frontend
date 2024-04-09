8380ae722fb38efa76faec7994ae509c3eaf1ff7
ansaf <ansafansar2001@gmail.com>
2024-04-09 13:56:25 +0530

    add choice selection option

--- frontend-client/README.md
+++ frontend-client/README.md
@@ -6,3 +6,34 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+check status command - git status
+check branch command - git branch
+switch branch command - git checkout <branch name>
+delete branch command - git branch -d <branch name>
+
+add a single file - git add <file-name>
+add files to commit - git add .
+commit to git branch - git commit -m <"your comments">
+push command - git push origin <branch name>
+
+
+
+
+//--------------------------------------------------
+Merge Back to Main Branch:
+If you want to push your changes back to the main branch, switch to the main branch:
+git checkout main
+
+Merge your branch into the main branch:
+git merge <your_branch_name>
+
+Push to the Online Repository:
+Push your changes to the online repository:
+git push
+
+For the first time pushing any branch, use:
+git push --set-upstream <repository_url> <your_branch_name>
--- frontend-client/component/question/AddQuestion.jsx
+++ frontend-client/component/question/AddQuestion.jsx
@@ -78,10 +78,95 @@ const AddQuestion = () => {
         if(newSubject.trim() !==""){
             setSubjectOptions(newSubject.trim())
             setSubjectOptions([...subjectOptions, newSubject.trim()])
+            setNewSubject("")
         }
     }
   return (
-    <div>
+    <div class='container'>
+        <div className='row justify-content-center'>
+            <div className='col-md-6 mt-5'>
+                <div className='card'>
+                    <div className='card-header'>
+                        <h5 className='card-title'>Add New Question</h5>
+                    </div>
+                    <div className='card-body'>
+                        <form onSubmit={handleSubmit} className='p-2'>
+                            <div className='mb-3'>
+                                <label htmlFor='subject' className='form-label text-info'>
+                                    Select a subject
+                                </label>
+                                <select
+                                    id="subject"
+                                    value={subject}
+                                    onChange={(e) => setSubject(e.target.value)}
+                                    className="form-control">
+                                    <option>Select a Subject</option>
+                                    <option value={New}>Add New Subject</option>
+                                    {subjectOptions.map((option) => (
+                                        <option key={option} value={option}>
+                                            {option}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {subject === "New" && (
+                                <div className='mb-3'>
+                                    <label htmlFor='newSubject' className='form-label text-info'>
+                                        Add a New Subject
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="new-subject"
+                                        value={newSubject}
+                                        onChange={(e) => setNewSubject(e.target.value)}
+                                        className="form-control"
+                                    />
+                                    <button
+                                        type="button"
+                                        className="btn btn-outline-primary btn-sm mt-2"
+                                        onClick={handleAddSubject}>
+                                    Add Subject
+                                    </button>
+                                </div>
+                            )}
+
+                            <div className="mb-2">
+                                <label htmlFor="question" className="form-label text-info">
+                                    Question
+                                </label>
+                                <textarea
+                                className="form-control"
+                                rows={4}
+                                value={question}
+                                onChange={(e) => setQuestion(e.target.value)}
+                                >
+                                </textarea>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="question-type" className="form-label text-info">
+                                    Question Type
+                                </label>
+                                <select
+                                    className="form-control"
+                                    id="question-type"
+                                    value={questionType}
+                                    onChange={(e) => setQuestionType(e.target.value)}>
+                                    
+                                    <option value={"single"}>Single Answer</option>
+                                    <option value={"multiple"}>Multiple Answer</option>
+                                </select>
+                            </div>
+
+
+                        </form>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
 
     </div>
   )
