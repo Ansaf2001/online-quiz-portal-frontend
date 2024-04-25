c0f18bc70d6139f745fbf3af7a63801b19964f14
ansaf <ansafansar2001@gmail.com>
2024-04-25 22:09:32 +0530

    adding Quiz.jsx file

--- frontend-client/component/quiz/Quiz.jsx
+++ frontend-client/component/quiz/Quiz.jsx
@@ -8,7 +8,7 @@ const Quiz = () => {
         correctAnswers: "",
         questionType: ""
     }])
-    const[selectedAnswers, setSelectedAnswers] = useState([{id: "", answer: ""}])
+    const[selectedAnswers, setSelectedAnswers] = useState([{id: "", answer: [""]}])
     const[currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
     const[totalScores, setTotalScores] = useState(0)
     const location = useLocation()
@@ -28,11 +28,137 @@ const Quiz = () => {
     }
 
     const handleAnswerChange = (questionId, answer) => {
-        setSelectedAnswers()
+        setSelectedAnswers((prevAnswers) => {
+            const existingAnswer = prevAnswers.findIndex((answerObj) => answerObj.id === questionId)
+            const selectedAnswer = Array.isArray(answer) ? answer.map((a) => a.charAt(0)) : answer.charAt(0)
+
+            if(existingAnswerIndex !== -1){
+                const updatedAnswers = [...prevAnswers]
+                updatedAnswers[existingAnswerIndex] = {id: questionId, answer: selectedAnswer }
+                return updatedAnswers
+            }
+            else {
+                const newAnswer = {id: questionId, answer: selectedAnswer}
+                return [...prevAnswers, newAnswer]
+            }
+        })
     }
 
+    const isChecked = (questionId, choice) =>{
+        const selectedAnswer = selectedAnswers.find((answer) => answer.id === questionId)
+        if (!selectedAnswer) {
+            return false
+        }
+        if(Array.isArray(selectedAnswer.answer)){
+            return selectedAnswer.answer.includes(choice.charAt(0))
+        }
+        return selectedAnswer.answer === choice.charAt(0)
+    }
+
+    const handleCheckboxChange = (questionId, choice) => {
+        setSelectedAnswers((prevAnswers) => {
+            const existingAnswerIndex = prevAnswers.findIndex((answerObj) => answerObj.id === questionId)
+            const selectedAnswer = Array.isArray(choice) ? choice.map((c) => c.charAt(0)) : choice.charAt(0)
+
+            if(existingAnswerIndex !== -1){
+                const updatedAnswers = [...prevAnswers]
+                const existingAnswers = updatedAnswers[existingAnswerIndex].answer
+                let newAnswer
+                if(Array.isArray(existingAnswers)){
+                    newAnswer = existingAnswers.includes(selectedAnswer) ? existingAnswers.filter((a) => a !== selectedAnswer)
+                    :[...existingAnswers, selectedAnswer]
+                }else{
+                    newAnswer = [existingAnswers, selectedAnswer]
+                }
+                updatedAnswers[existingAnswerIndex] = {id: questionId, answer: newAnswer }
+                return updatedAnswers
+            }else{
+                const newAnswer = {id: questionId, answer: [selectedAnswer]}
+                return [...prevAnswers, newAnswer]
+            }
+        })
+    }
+
+    const handleSubmit = () => {
+        let scores = 0;
+        quizQuestions.forEach((question) => {
+            const selectedAnswer = selectedAnswers.find((answer) => answer.id === question.id)
+            if(selectedAnswer){
+                const selectedOptions = Array.isArray(selectedAnswer.answer)
+                 ? selectedAnswer.answer : [selectedAnswer.answer]
+                 const correctOptions = Array.isArray(question.correctAnswers)
+                  ? question.correctAnswers : [question.correctAnswers]
+                  const isCorrect = selectedOptions.every((option) => 
+                  correctOptions.includes(option))
+                  if(isCorrect){
+                    scores++
+                  }
+            }
+    })
+    setTotalScores(scores)
+    setSelectedAnswers([{id: "", answer: [""]}])
+    setCurrentQuestionIndex(0)
+    navigate("/quiz-result", {state:{quizQuestions, totalScores:scores}})
+    }
+
+    const handleNextQuestion = () => {
+        if(correctQuestionIndex < quizQuestions.length -1){
+            setCurrentQuestionIndex((prevIndex) => prevIndex + 1)
+        }
+        else{
+            handleSubmit()
+        }
+    }
+
+    
+    const handlePreviousQuestion = () => {
+        if(correctQuestionIndex > 0){
+            setCurrentQuestionIndex((prevIndex) => prevIndex - 1)
+        }
+       
+    }
+
+
   return (
-    <div>
+    <div className='p-5'>
+        <h3 className='text-info'>
+            Question{quizQuestions.length > 0 
+            ? currentQuestionIndex + 1 : 0} of {quizQuestions.length}
+        </h3>
+
+   
+<h4 className='mb-4'>
+    {quizQuestions[currentQuestionIndex]?.question}
+
+    <AnswerOptions
+        question={quizQuestions[currentQuestionIndex]}
+        isChecked={isChecked}
+        handleAnswerChange={handleAnswerChange}
+        handleCheckboxChange={handleCheckboxChange}
+    />
+
+    <div className='mt-4'>
+        <button
+            className='btn btn-sm btn-primary me-2'
+            onClick={handlePreviousQuestion}
+            disabled={currentQuestionIndex === 0}
+        >
+            Previous Question
+        </button>
+
+        <button
+            className={`btn btn-sm btn-info ${
+                currentQuestionIndex === quizQuestions.length - 1 ? "btn btn-sm btn-warning" : ""
+            }`}
+            onClick={handleNextQuestion}
+            disabled={!selectedAnswers.find(answer =>
+                answer.id === quizQuestions[currentQuestionIndex]?.id || answer.answer.length > 0
+            )}
+        >
+            {currentQuestionIndex === quizQuestions.length - 1 ? "Submit Quiz" : "Next Question"}
+        </button>
+    </div>
+</h4>
 
     </div>
   )
--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReactDOM from 'react-dom'
+// import "index.css"
+// import AddQuestion from '../component/question/AddQuestion.jsx'
+// import App from './App.jsx'/
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
